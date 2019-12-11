0cd311c2dd09767555db00cf18a0f99c6499de20
Bryn Bayliss <brynbayliss@hotmail.com>
2019-12-11 11:12:00 +0000

    day 3, part 1: vector slices

--- day_3/main.go
+++ day_3/main.go
@@ -6,6 +6,7 @@ import (
   "fmt"
   "log"
   "strings"
+  "strconv"
 )
 
 func main() {
@@ -32,13 +33,44 @@ func main() {
   line_1 = strings.Split(line_1[0], ",")
   line_2 = strings.Split(line_2[0], ",")
 
+  line_1_vectors := create_vectors(line_1)
+  line_2_vectors := create_vectors(line_2)
+
+  fmt.Println("line 1:", line_1_vectors)
+  fmt.Println("line 2:", line_2_vectors)
 
 }
 
+type vector struct {
+  x int
+  y int
+}
 
-func create_vectors(directions []string)  [][]string {
-  var vectors = []string{}
-  for i := range directions {
-    if i[0] == "L" || "R" {
-      vector := ["0", i[
+func create_vectors(directions []string)  []vector {
+  var all_vectors = []vector{}
+  var a_vector = vector{}
+  var last_vector = vector{}
+  all_vectors = append(all_vectors, last_vector)
+  for _, i := range directions {
+    axis := string(i[0])
+    last_vector = all_vectors[len(all_vectors) -1]
+    if axis == "L" || axis == "R" {
+      y, err := strconv.Atoi(i[1:len(i)])
+      if err != nil {
+        log.Fatal("err")
+      }
+      a_vector = vector{x: last_vector.x, y: last_vector.y + y}
+    } else {
+      x, err := strconv.Atoi(i[1:len(i)])
+      if err != nil {
+        log.Fatal("err")
+      }
+
+      a_vector = vector{x: last_vector.x + x, y: last_vector.y}
+    }
+    all_vectors = append(all_vectors, a_vector)
+  }
+  return all_vectors
+}
+
 
